3e1a58bb6e55b4a22690b7736a75d733dafe1d0f
NeoGeo128 <jesse.yannelli@uky.edu>
2013-03-26 02:57:26 -0400

    up to date for meeting

--- cs499/cs499_app/views/api/viewFiles.py
+++ cs499/cs499_app/views/api/viewFiles.py
@@ -5,16 +5,36 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import MySQLdb
 
+# @csrf_exempt
+# def user_files(request):
+#     state = "User's files" 
+#     direct = "files.html"
+# 	try:
+# 		userfiles = UserFiles.objects.all().order_by('numFiles')
+# 		filename = ','.join([f.filename for f in uesrfiles])
+		
+# 	except UserFiles.DoesNotExist:
+# 		state = "You do not currently have any files"
+# 		direct = "files.html"
+
+# 	return render_to_response(direct,{'state':state, 'filename':userfiles})    
+
 @csrf_exempt
 def user_files(request):
     state = "User's files" 
     direct = "files.html"
 	try:
-		userfiles = UserFiles.objects.all().order_by('numFiles')
+		userfiles = Session.objects.all().order_by('id')
 		filename = ','.join([f.filename for f in uesrfiles])
 		
 	except UserFiles.DoesNotExist:
 		state = "You do not currently have any files"
 		direct = "files.html"
 
-	return render_to_response(direct,{'state':state, 'filename':userfiles})    
+	return render_to_response(direct,{'state':state, 'filename':userfiles})  
+	
+def viewSessions(request, query_params=None, **kwargs):
+	events = MotionEvent.objects.all()
+	retVal = {
+		'events': [e.to_dict() for e in events]	
+	};
--- cs499/urls.py
+++ cs499/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^api/', include('cs499.cs499_app.views.api.urls')),
+    #url(r'^api/(?P<username>\d+)/viewFiles/$', 'api.viewFiles.viewSession') 
     #url(r'^login/$','cs499.cs499_app.views.api.auth.views.login_user'),
 
 )
