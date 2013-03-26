52688b6838b9727f9ffccb48acc0165ee988baa0
NeoGeo128 <jesse.yannelli@uky.edu>
2013-03-26 02:01:07 -0400

    Added database test file, open in postgres and run to insert test data

---
+++ cs499/db_test_files/test data.sql
@@ -0,0 +1,35 @@
+﻿INSERT INTO cs499_app_device VALUES
+    (1, '1MER234F34GM12','SAMSUNG NOTE' );
+INSERT INTO cs499_app_device VALUES
+    (2, 'RTH34534RH3','NEXUS 4' );
+INSERT INTO cs499_app_device VALUES
+    (3, '342HTYSD2341IIM','NEXUS 7' );
+
+#SELECT * FROM cs499_app_device;
+
+#SELECT * FROM cs499_app_loginusers;
+
+INSERT INTO cs499_app_loginusers VALUES
+	(1, 'jjyann2', 'hibiki');
+INSERT INTO cs499_app_loginusers VALUES
+	(2, 'hshiner2', 'password');
+INSERT INTO cs499_app_loginusers VALUES
+	(3, 'jcyan2', 'password2');
+
+#SELECT * FROM cs499_app_motionevent;
+
+INSERT INTO cs499_app_motionevent VALUES
+	(1, 23,34,'12:00:09',2);
+INSERT INTO cs499_app_motionevent VALUES
+	(2, 25,35,'12:00:10',2);
+INSERT INTO cs499_app_motionevent VALUES
+	(3, 30,35,'12:00:12',5);		
+
+#SELECT * FROM cs499_app_session;
+
+INSERT INTO cs499_app_session VALUES
+	(1, 1,1,1,'2013-03-20 04:05:06');	
+INSERT INTO cs499_app_session VALUES
+	(2, 1,1,1,'2013-03-20 05:05:00');	
+INSERT INTO cs499_app_session VALUES
+	(3, 2,1,1,'2013-03-25 04:05:06');	
