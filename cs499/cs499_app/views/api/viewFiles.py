975511b726aeeb059d7244251e83ae5eac4d036c
hashin2 <hashin2@g.uky.edu>
2013-04-04 14:50:14 -0400

    DB query working, able to show filename list in HTML page

--- cs499/cs499_app/views/api/viewFiles.py
+++ cs499/cs499_app/views/api/viewFiles.py
@@ -18,13 +18,29 @@ def user_files(request):
 def __user_files(request, query_params=None, **kwargs):
 	# events = MotionEvent.objects.all()
 	events = UserFiles.objects.all()
-	retVal = {
-		'Files': [e.to_dict() for e in events],		
+	# retVal = {
+	# 	'Files': [e.to_dict() for e in events],		
 
-	};
+	# };
+	results = []
+	for e in events:
+		results.append(e.to_dict())
 
-	return HttpResponse(jsonify(retVal, query_params), mimetype="application/json")
+
+
+	# filename = jsonify(retVal, query_params)
+	# files = "<tr><td><b> %s </b></td><td>" %filename
+	# return HttpResponse(jsonify(retVal, query_params), mimetype="application/json")
+	# return render_to_response('files.html', jsonify(retVal, query_params)) 	
 	
+	# from django.template import Context, Template
+	# f = Template("File: {{html}}")
+	# c = Context({"html": files})
+	# f.render(jsonify(retVal, query_params));
+
+	# return render_to_response('files.html',{'data' :retVal.iteritems()})
+	return render_to_response('files.html',{'data' :results})
+
 def viewSessions(request, query_params=None, **kwargs):
 	events = MotionEvent.objects.all()
 	retVal = {
